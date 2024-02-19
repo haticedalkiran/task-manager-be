6f1dfac68f1d3cbe8ae5c357b5c22cafd082a990
Hatice Dalkıran <kocakkhatice@gmail.com>
2024-02-19 16:51:28 +0300

    last commit

--- models/taskSchema.js
+++ models/taskSchema.js
@@ -1,10 +1,17 @@
 const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema({
+  _id: String,
   title: String,
   detail: String,
+  status: String,
+  reporter: String,
   assignee: String,
   dueDate: String,
+  createdAt: String,
+
+  updatedAt: String || null,
+  updatedBy: String || null,
 });
 
 module.exports = mongoose.model("Task", taskSchema);
--- socket.js
+++ socket.js
@@ -4,15 +4,16 @@ const http = require("http").createServer(app);
 const { Server } = require("socket.io");
 const cors = require("cors");
 const mongoose = require("mongoose");
+
 const Task = require("./models/taskSchema");
-const { ObjectId } = require("mongodb");
+const User = require("./models/userSchema");
 
 mongoose.connect("mongodb://localhost:27017/task-manager-db", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-let mongooseConn = mongoose.connection;
+app.use(express.json());
 
 const io = new Server(http, {
   cors: {
@@ -37,6 +38,60 @@ app.get("/", (req, res) => {
   res.end("hello socket");
 });
 
+app.post("/signup", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    User.init();
+    const user = await User.findOne({ username, password });
+
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const newUser = new User({ username, password });
+    newUser.save().then(() =>
+      res.status(201).json({
+        message: "success",
+        user: { _id: newUser._id, username: newUser.username },
+      })
+    );
+  } catch (e) {
+    res.status(500).json({ message: "error", e });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    User.init();
+    const user = await User.findOne({ username, password });
+
+    if (user) {
+      res.status(200).json({
+        message: "Success",
+        user: { _id: user._id, username: user.username },
+      });
+      return;
+    }
+
+    res.status(401).json({ message: "Wrong name or password." });
+  } catch (e) {
+    res.status(500).json({ message: "Error" });
+  }
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    User.init();
+    const users = await User.find({});
+    res
+      .status(200)
+      .json(users.map((user) => ({ _id: user._id, username: user.username })));
+  } catch (e) {
+    console.log("error", e);
+    res.status(500).json({ message: e });
+  }
+});
+
 io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
 
@@ -49,52 +104,34 @@ io.on("connection", (socket) => {
     console.log("error", error);
   }
 
-  socket.on("new-task", (task) => {
-    mongooseConn.collection("tasks").insertOne(task, (err, res) => {
-      if (err) throw err;
-      console.log("task added successfully");
-    });
+  socket.on("new-task", (taskData) => {
+    console.log("new-task", taskData);
+    const task = new Task(taskData);
+
+    task
+      .save()
+      .then((savedTask) => {
+        console.log("savedTask", savedTask);
+        io.emit("task-created", savedTask);
+      })
+      .catch((error) => {});
+  });
+
+  socket.on("delete-task", async (taskId) => {
     try {
-      const tasks = Task.find({});
-      tasks.then((data) => {
-        socket.broadcast.emit("receive-tasks", data);
-      });
-    } catch (error) {
-      console.log("error", error);
-    }
+      await Task.findByIdAndDelete(taskId);
+      io.emit("task-deleted", taskId);
+    } catch (error) {}
   });
 
-  socket.on("delete-task", (id) => {
-    mongooseConn
-      .collection("tasks")
-      .deleteOne({ _id: new ObjectId(id) }, (err, res) => {
-        if (err) throw err;
-        console.log("task deleted successfully", res);
-        const tasks = Task.find({});
-        tasks.then((data) => {
-          socket.broadcast.emit("receive-tasks", data);
-        });
+  socket.on("update-task", async (taskData) => {
+    try {
+      const updatedTask = await Task.findByIdAndUpdate(taskData._id, taskData, {
+        new: true,
       });
+      io.emit("task-updated", updatedTask);
+    } catch (error) {}
   });
-  socket.on("update-task", (task) => {
-    console.log("gelen task", task);
-    mongooseConn.collection("tasks").updateOne(
-      { _id: new ObjectId(task.id) }, //TODO
-      { $set: { ...task } },
-      (err, result) => {
-        if (err) {
-          console.error("Update error:", err);
-        } else {
-          console.log("Update success:", result);
-          const tasks = Task.find({});
-          tasks.then((data) => {
-            socket.broadcast.emit("receive-tasks", data);
-          });
-        }
-      }
-    );
-  });
-
   socket.on("disconnect", () => {
     console.log("user disconnected", socket.id);
   });
